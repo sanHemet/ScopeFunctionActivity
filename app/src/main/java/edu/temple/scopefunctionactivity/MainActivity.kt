ece6655a4e5b9a2dc938f5a5ef5d031c85749048
sanHemet <20hhemet@gmail.com>
2024-02-28 16:30:38 -0500

    getView converted to single expression

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 //        return testArray
 //    }
 
-    private fun getTestDataArray() = MutableList(10) { Random.nextInt() }.apply { sort() }
+    private fun getTestDataArray() = MutableList(10) { Random.nextInt() }.sorted()
 
     // Return true if average value in list is greater than median value, false otherwise
 //    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
@@ -60,20 +60,28 @@ class MainActivity : AppCompatActivity() {
         }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
+//    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
+//        val textView: TextView
+//
+//        if (recycledView != null) {
+//            textView = recycledView as TextView
+//        } else {
+//            textView = TextView(context)
+//            textView.setPadding(5, 10, 10, 0)
+//            textView.textSize = 22f
+//        }
+//
+//        textView.text = collection[position].toString()
+//
+//        return textView
+//    }
 
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
-        }
-
-        textView.text = collection[position].toString()
-
-        return textView
-    }
-
+    //x ?: y -> 'x' if 'x' is not null, otherwise 'y'
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context) =
+        (recycledView?.run{ (this as TextView) } ?: TextView(context).apply{
+            this.setPadding(5,10,10,0)
+            this.textSize = 22f
+        }).apply {
+            this.text = collection[position].toString()
+        } //parenthesis needed, because second apply would just be the same as putting it inside, close first, then apply after
 }
